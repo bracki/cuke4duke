aaf44293c3559c0de4517a55964881e201c17dd0
bracki <jb@rootscommandment.de>
2010-01-29 23:48:24 +0100

    Adding a nice simple test.

--- cuke4duke-maven-plugin/src/test/java/cuke4duke/mojo/CucumberMojoTest.java
+++ cuke4duke-maven-plugin/src/test/java/cuke4duke/mojo/CucumberMojoTest.java
@@ -3,6 +3,7 @@ package cuke4duke.mojo;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.DefaultArtifactRepository;
@@ -29,6 +30,21 @@ public class CucumberMojoTest {
         mojo.pluginArtifacts = new ArrayList<Artifact>();
         mojo.testClasspathElements = new ArrayList<String>();
         mojo.localRepository = new DefaultArtifactRepository("", "", new DefaultRepositoryLayout());
+    }
+
+    @Test
+    public void shouldBuildGemFileFromGems() throws Exception {
+        mojo.gems = new ArrayList<Gem>();
+        Gem g = new Gem();
+        g.setName("gem");
+        g.setVersion("1.2.3");
+        mojo.gems.add(g);
+        File gemFile = mojo.gemFileFromGems();
+        assertTrue(gemFile.exists());
+        Scanner scanner = new Scanner(gemFile).useDelimiter("\\Z");
+        String contents = scanner.next();
+        scanner.close();
+        assertTrue(contents.matches("gem \"gem\", \"1\.2\.3\"");
     }
 
     @Test
